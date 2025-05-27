f12534d80cda66b57dccd6debd8914495c9f2bab
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-05-27 11:52:17 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from mymodule import add
+# tests/test_main.py
+
+from mymodules import add
+
+def test_add():
+    assert add(2, 3) == 5
+
 
